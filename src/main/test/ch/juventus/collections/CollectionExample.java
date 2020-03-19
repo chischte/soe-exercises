243027d20dd75d8fc1942b8232ca20f758340d16
Michael Wettstein <chischte@gmx.ch>
2020-03-19 17:38:43 +0100

    Add class CollectionExample

---
+++ src/main/test/ch/juventus/collections/CollectionExample.java
@@ -0,0 +1,15 @@
+package ch.juventus.collections;
+
+public class CollectionExample {
+
+
+
+    public void createList(){
+
+    }
+
+    public void printList(){
+
+    }
+
+}
---
+++ src/main/test/ch/juventus/collections/Main.java
@@ -0,0 +1,10 @@
+package ch.juventus.collections;
+
+public class Main {
+
+    public static void main(String[] args){
+        CollectionExample collectionExample=new CollectionExample();
+
+    }
+
+}
