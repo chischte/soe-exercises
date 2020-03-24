095e185fb0fa3daa3264dae2429568ad5eba6188
Michael Wettstein <chischte@gmx.ch>
2020-03-24 23:37:58 +0100

    Create class StringSplitter

---
+++ src/main/java/ch/juventus/stringsplitter/Main.java
@@ -0,0 +1,7 @@
+package ch.juventus.stringsplitter;
+
+public class Main {
+    public static void main(String[] args){
+        StringSplitter stringSplitter = new StringSplitter();
+    }
+}
---
+++ src/main/java/ch/juventus/stringsplitter/StringSplitter.java
@@ -0,0 +1,9 @@
+package ch.juventus.stringsplitter;
+
+public class StringSplitter {
+
+    public String getFirstLetter(String word) {
+        return word;
+    }
+
+}
