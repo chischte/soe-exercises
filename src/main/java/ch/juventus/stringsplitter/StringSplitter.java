90783395510afcde1a31961d0d839812ebd5430a
Michael Wettstein <chischte@gmx.ch>
2020-03-25 00:09:08 +0100

    Add a log to string splitter

--- src/main/java/ch/juventus/stringsplitter/StringSplitter.java
+++ src/main/java/ch/juventus/stringsplitter/StringSplitter.java
@@ -1,9 +1,16 @@
 package ch.juventus.stringsplitter;
 
+import ch.juventus.logging.BankAccount;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StringSplitter {
+
+    final Logger logger = LoggerFactory.getLogger(StringSplitter.class);
 
     public char getFirstChar(String word) {
         char firstChar = word.charAt(0);
+        logger.info("Got first char of {} result: {}", word, firstChar);
         return firstChar;
     }
 
