88622ef8e553d16f857f77444730d3f8bda88a3f
Michael Wettstein <chischte@gmx.ch>
2020-04-02 19:48:39 +0200

    Add exception and try catch block

--- src/main/java/ch/juventus/exceptions/Main.java
+++ src/main/java/ch/juventus/exceptions/Main.java
@@ -1,15 +1,30 @@
 package ch.juventus.exceptions;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-    PersonRepository personRepository=new PersonRepository();
-        personRepository.addPerson(1,"Hans");
-        personRepository.addPerson(2,"Klara");
+        PersonRepository personRepository = new PersonRepository();
+
+        // Add persons to repository:
+        personRepository.addPerson(2, "Klara");
+
+        // Optional catch IllegalArgumentException:
+        try {
+            personRepository.addPerson(null, "Hans");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Illegal argument exception");
+        }
+
         personRepository.addPerson(3, "Robert");
 
         System.out.println(personRepository.getPersonRegister());
-        System.out.println(personRepository.getPersonName(1));
-        System.out.println(personRepository.getPersonName(4));
+        //System.out.println(personRepository.getPersonName(1));
+        //System.out.println(personRepository.getPersonName(4));
+
+        try {
+            personRepository.getPersonName(4);
+        } catch (PersonNotFoundException e) {
+            System.out.println("Invalid Person Id exception");
+        }
     }
 }
--- src/main/java/ch/juventus/exceptions/PersonRepository.java
+++ src/main/java/ch/juventus/exceptions/PersonRepository.java
@@ -2,20 +2,28 @@ package ch.juventus.exceptions;
 
 import java.util.HashMap;
 
-public class PersonRepository {
+public class PersonRepository extends Exception {
 
     HashMap<Integer, String> personRegister = new HashMap<Integer, String>();
 
     public void addPerson(Integer personId, String personName) {
         this.personRegister.put(personId, personName);
+        if (personId == null) {
+            throw new IllegalArgumentException("Person Id should not be null");
+        }
     }
 
     public HashMap<Integer, String> getPersonRegister() {
         return personRegister;
     }
 
-    public String getPersonName(Integer id) {
+    public String getPersonName(Integer id) throws PersonNotFoundException {
+        if (this.personRegister.get(id) == null) {
+            throw new PersonNotFoundException("Person not found Exception");
+        }
+
         return this.personRegister.get(id);
     }
+
 
 }
