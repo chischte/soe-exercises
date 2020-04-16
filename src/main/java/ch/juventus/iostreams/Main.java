b7e0fe6fdbbf274faaec707405fac7404d682708
Michael Wettstein <chischte@gmx.ch>
2020-04-16 19:21:38 +0200

    Add a String in the method signature

--- src/main/java/ch/juventus/iostreams/Main.java
+++ src/main/java/ch/juventus/iostreams/Main.java
@@ -2,7 +2,8 @@ package ch.juventus.iostreams;
 
 public class Main {
     public static void main(String[] args) {
-    MyWriter mywriter = new MyWriter();
-    MyReader myreader = new MyReader();
-}
+        MyWriter mywriter = new MyWriter();
+        mywriter.write("HAUDI");
+        MyReader myreader = new MyReader();
+    }
 }
--- src/main/java/ch/juventus/iostreams/MyWriter.java
+++ src/main/java/ch/juventus/iostreams/MyWriter.java
@@ -8,17 +8,21 @@ public class MyWriter {
 
     Writer myWriter;
 
-    {
-        try {
-            myWriter = new FileWriter("src//main/java/ch/juventus/iostreams/myFile.txt");
-            StringBuffer outputText = new StringBuffer();
-            outputText.append("Write NewHaudi");
-            myWriter.write(outputText.toString());
-            myWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void write(String messageToWrite) {
+        {
+            try {
+                myWriter = new FileWriter("src//main/java/ch/juventus/iostreams/myFile.txt");
+                StringBuffer outputText = new StringBuffer();
+                outputText.append("This is the received message:  ");
+                outputText.append(messageToWrite);
+                myWriter.write(outputText.toString());
+                myWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
-
-
 }
+
+
+
