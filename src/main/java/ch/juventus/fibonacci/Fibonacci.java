12f3a1dc1e040e7a9a8b4b4185315292f3f15c96
Michael Wettstein <chischte@gmx.ch>
2020-06-07 12:51:23 +0200

    Add package fibonacci

---
+++ src/main/java/ch/juventus/fibonacci/Fibonacci.java
@@ -0,0 +1,11 @@
+package ch.juventus.fibonacci;
+
+public class Fibonacci {
+
+    public int calculateFibonacciNumber(int fibonacciStep ){
+        int fibonacciSum=666;
+
+        return fibonacciSum;
+    }
+
+}
---
+++ src/main/java/ch/juventus/fibonacci/Main.java
@@ -0,0 +1,13 @@
+package ch.juventus.fibonacci;
+
+public class Main {
+
+
+    public static void main(String[] args){
+
+    Fibonacci fibonacci = new Fibonacci();
+        int fibonacciNumber=fibonacci.calculateFibonacciNumber(10);
+        System.out.println(fibonacciNumber);
+    }
+
+}
