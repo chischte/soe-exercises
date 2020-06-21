8f41cf50ccb984eab1afcb903f8aef262b04539f
Michael Wettstein <chischte@gmx.ch>
2020-06-21 11:54:13 +0200

    Add server and client, successful remote connection via ddns

--- src/main/java/ch/juventus/socketproject/client/MainClient.java
+++ src/main/java/ch/juventus/socketproject/client/MainClient.java
@@ -1,8 +1,17 @@
 package ch.juventus.socketproject.client;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MainClient {
     public static void main(String[] args) {
-        
+        try {
+            Socket client = new Socket("machinelogger.synology.me", 8888);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
--- src/main/java/ch/juventus/socketproject/server/MainServer.java
+++ src/main/java/ch/juventus/socketproject/server/MainServer.java
@@ -1,8 +1,18 @@
 package ch.juventus.socketproject.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MainServer {
 
     public static void main(String[] args){
+        try {
+            ServerSocket server = new ServerSocket(8888);
+            Socket client=server.accept();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
